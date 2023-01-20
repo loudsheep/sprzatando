d5530c9c7e54c90747d7b27d3b6e410d47034754
Patryk <patryk.kuzdrowski@gmail.com>
2023-01-20 17:58:51 +0100

    github connection test

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,7 +13,7 @@ export default function Dashboard(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">This is the github connection test</div>
                     </div>
                 </div>
             </div>
