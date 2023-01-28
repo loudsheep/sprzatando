2afd9b01cd9a6ea07044ddff1349d9b308afd0b9
loudsheep <loudsheep05@gmail.com>
2023-01-28 21:19:24 +0100

    Moved styled components above declaration

--- resources/js/Components/Atoms/Button.jsx
+++ resources/js/Components/Atoms/Button.jsx
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 
+const StyledButton = styled.button`
+  padding: 1rem 2rem;
+  font-size: 1.6rem;
+  background-color: ${({theme}) => theme.colors.secondaryColor};
+  color: #fff;
+  border-radius: 8px;
+  transition: opacity .3s;
+
+  :hover{
+    opacity: .9;
+  }
+`;
 const Button = (props) => {
-  const Button = styled.button`
-    padding: 1rem 2rem;
-    font-size: 1.6rem;
-    background-color: ${({theme}) => theme.colors.secondaryColor};
-    color: #fff;
-    border-radius: 8px;
-    transition: opacity .3s;
 
-    :hover{
-      opacity: .9;
-    }
-  `;
-
-  return <Button>{props.text}</Button>;
+  return <StyledButton>{props.text}</StyledButton>;
 };
 
 export default Button;
