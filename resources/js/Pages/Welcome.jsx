25d9659f6fe7f162d18eafc761c637a8130e99a8
Patryk <patryk.kuzdrowski@gmail.com>
2023-01-29 20:57:32 +0100

    Display offers in Welcome Section

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,17 @@
 import { Head } from "@inertiajs/react";
 import styled from "styled-components";
+import { Offer } from "@/Components/Offer";
 import { Navbar } from "@/Components/Navbar";
 import FilterSection from "@/Components/FilterSection";
+
+const OfferWrapper = styled.div`
+  width: 100%;
+  margin-top: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 export default function Welcome({ auth, cities, offers }) {
   return (
@@ -11,10 +21,18 @@ export default function Welcome({ auth, cities, offers }) {
         <Navbar auth={auth} />
         <FilterSection />
       </header>
-
-      {offers.map((offer) => (
-        <div>City - {offer.city}</div>
-      ))}
+      <OfferWrapper>
+        {offers.map((offer, i) => (
+          <Offer
+            title={offer.category}
+            description={offer.description}
+            hourlyRate={offer.hourly_rate}
+            category={offer.category}
+            city={offer.city}
+            key={i}
+          />
+        ))}
+      </OfferWrapper>
     </>
   );
 }
