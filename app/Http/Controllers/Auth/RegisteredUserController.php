0679f10e4867700439b006a3bbcf6a4431560f44
loudsheep <loudsheep05@gmail.com>
2023-02-02 15:42:36 +0100

    Sending verification email when user registers

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Carbon\Carbon;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -33,7 +36,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -42,6 +45,21 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        // send email verification when register
+        $emailUrl = URL::temporarySignedRoute(
+            "verification.verify",
+            Carbon::now()->addMinutes(60),
+            [
+                "id" => $user->id,
+                "hash" => sha1($user->email),
+            ]
+        );
+
+        Mail::send('emails.verify', ["url" => $emailUrl], function ($message) use ($user) {
+            $message->to($user->email);
+            $message->subject("Zweryfikuj swój adres email");
+        });
 
         event(new Registered($user));
 
