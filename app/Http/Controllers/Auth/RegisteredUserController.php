6167926e0a9996c0ed9cea3a32fabc6f40309edf
loudsheep <loudsheep05@gmail.com>
2023-02-13 17:54:29 +0100

    Ascii email validation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,7 +36,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255|unique:' . User::class,
-            'email' => 'required|string|email|max:255|unique:' . User::class,
+            'email' => 'required|ascii|string|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
