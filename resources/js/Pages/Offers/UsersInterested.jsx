fb75fbd1b4584e99091e9986bd004b4306a2095c
Fabian <zandal.zdl@gmail.com>
2023-05-25 18:06:44 +0200

    Added responsivity to offer user intrested section

--- resources/js/Pages/Offers/UsersInterested.jsx
+++ resources/js/Pages/Offers/UsersInterested.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Button from "@/Components/Atoms/Button";
 import { Inertia } from "@inertiajs/inertia";
-import { useState } from 'react'
+import { useState } from "react";
 
 const StyledTitle = styled.h1`
   font-size: 2.6rem;
@@ -24,7 +24,7 @@ const Wrapper = styled.div`
 `;
 
 const InterestedWrapper = styled.div`
-  flex:  3;
+  flex: 3;
   margin-top: 4rem;
   display: flex;
   align-items: center;
@@ -35,7 +35,7 @@ const InterestedWrapper = styled.div`
 `;
 
 const DetailsWrapper = styled.div`
-  flex:  2;
+  flex: 2;
   margin-top: 4rem;
   display: flex;
   flex-direction: column;
@@ -96,7 +96,7 @@ const DetailsWrapper = styled.div`
 
 export const Review = styled.div`
   width: 100%;
-  margin: 2rem 10% 0 10%;  
+  margin: 2rem 10% 0 10%;
   display: flex;
   flex-direction: row;
 
@@ -121,38 +121,77 @@ export const Review = styled.div`
 const User = styled.div`
   max-width: 500px;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
   background-color: #fff;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   border: 1px solid ${({ theme }) => theme.colors.lightPurple};
   margin: 1rem;
-  padding-left: 1.5rem;
-  padding-right: 1.5rem;
+  padding: 3rem;
   border-radius: 15px;
-  height: 8rem;
-  width: 100%;
+
+  @media (min-width: 992px) {
+    width: auto;
+  }
 
   .pic {
-    height: 100%;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
+    height: 100%;
+    margin-bottom: 1rem;
     font-size: 2.5rem;
+
+    @media (min-width: 992px) {
+      justify-content: space-between;
+    }
   }
   .name {
-    width: 60%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .btn-wrapper > a{
+    justify-content: center;
+    flex-wrap: wrap;
+    .btn-wrapper > a {
       margin: 0 10px;
     }
   }
   img {
-    height: 70%;
+    width: 50px;
+    height: 50px;
     border-radius: 50%;
     margin-left: 20px;
+
+    @media (min-width: 768px) {
+      width: 75px;
+      height: 75px;
+    }
+  }
+
+  .bottom-wrapper {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 992px) {
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+
+  .button-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+
+  .username {
+    text-align: center;
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+
+    @media (min-width: 992px) {
+      margin-left: 2rem;
+      margin-bottom: 0;
+    }
   }
 `;
 
@@ -163,7 +202,7 @@ export default function UsersInterested({
   interestedUsers,
 }) {
   const handleUserSelect = (userId) => {
-    Inertia.post(route('offer.user.select', [offer.id, userId]));
+    Inertia.post(route("offer.user.select", [offer.id, userId]));
   };
 
   const [viewingUser, setViewingUser] = useState(null);
@@ -178,28 +217,31 @@ export default function UsersInterested({
         prophileImg={auth.user.profile_img}
       >
         <Wrapper>
-
-
           <InterestedWrapper>
             <StyledTitle>Chętni do oferty:</StyledTitle>
             {interestedUsers.map((user, i) => (
               <User key={i}>
                 <div className="pic">
-                  {i + 1}.
+                  <p>{i + 1}.</p>
                   <img
                     src={user.profile_img}
                     style={{ borderRadius: "50%" }}
                     alt="profile img"
                   />
                 </div>
-                <div className="name">
-                  <span>
-                    <strong>{user.name}</strong>
-                  </span>
-                  <div className="btn-wrapper">
-                    {/* <Button text={"TEXT"} onClick={() => setViewingUser(user)}>Szczegóły</Button> */}
-                    <b onClick={() => setViewingUser(user)} style={{ marginRight: "1rem", cursor: "pointer" }}>Szczegóły</b>
-                    <Button text="wybierz" onClick={() => handleUserSelect(user.id)} />
+                <div className="bottom-wrapper">
+                  <p className="username">{user.name}</p>
+                  {/* <Button text={"TEXT"} onClick={() => setViewingUser(user)}>Szczegóły</Button> */}
+                  <div className="button-wrapper">
+                    <Button
+                      onClick={() => setViewingUser(user)}
+                      style={{ marginRight: "1rem", cursor: "pointer" }}
+                      text="Szczegóły"
+                    />
+                    <Button
+                      text="wybierz"
+                      onClick={() => handleUserSelect(user.id)}
+                    />
                   </div>
                 </div>
               </User>
@@ -207,14 +249,17 @@ export default function UsersInterested({
 
             {interestedUsers.length == 0 && (
               <>
-                Brak
+                <p>Brak</p>
               </>
             )}
           </InterestedWrapper>
 
           {viewingUser != null && (
             <DetailsWrapper>
-              <div style={{ textAlign: "right", width: "100%", cursor: "pointer" }} onClick={() => setViewingUser(null)}>
+              <div
+                style={{ textAlign: "right", width: "100%", cursor: "pointer" }}
+                onClick={() => setViewingUser(null)}
+              >
                 X
               </div>
               <div className="name">
@@ -226,29 +271,39 @@ export default function UsersInterested({
               <div className="details">
                 <div className="row">
                   <div className="prop">Dołączył</div>
-                  <div className="value">{new Date(viewingUser.created_at).toLocaleDateString()}</div>
+                  <div className="value">
+                    {new Date(viewingUser.created_at).toLocaleDateString()}
+                  </div>
                 </div>
 
                 <div className="row">
                   <div className="prop">Średnia ocen</div>
-                  <div className="value" style={{ color: "#ffda09" }}>{Number(viewingUser.reviews_avg_rating).toFixed(1)}★</div>
+                  <div className="value" style={{ color: "#ffda09" }}>
+                    {Number(viewingUser.reviews_avg_rating).toFixed(1)}★
+                  </div>
                 </div>
 
                 <div className="row">
                   <div className="prop">Ilość wykonanych zleceń</div>
-                  <div className="value">{viewingUser.contracted_offers_count}</div>
+                  <div className="value">
+                    {viewingUser.contracted_offers_count}
+                  </div>
                 </div>
 
                 <div className="row">
                   <div className="prop">Ilość stworzonych zleceń</div>
-                  <div className="value">{viewingUser.created_offers_count}</div>
+                  <div className="value">
+                    {viewingUser.created_offers_count}
+                  </div>
                 </div>
 
                 {viewingUser.latest_review != null && (
                   <>
                     <div className="row">
                       <div className="prop">Ostatnia ocena</div>
-                      <div className="value" style={{ color: "#ffda09" }}>{viewingUser.latest_review.rating}★</div>
+                      <div className="value" style={{ color: "#ffda09" }}>
+                        {viewingUser.latest_review.rating}★
+                      </div>
                     </div>
 
                     <Review>
@@ -259,7 +314,9 @@ export default function UsersInterested({
                       <div className="description">
                         <b>
                           {viewingUser.latest_review.offer.creator.name} -{" "}
-                          <span style={{ color: "#ffda09" }}>{viewingUser.latest_review.rating} ★</span>
+                          <span style={{ color: "#ffda09" }}>
+                            {viewingUser.latest_review.rating} ★
+                          </span>
                         </b>
 
                         {viewingUser.latest_review.description}
@@ -270,7 +327,6 @@ export default function UsersInterested({
               </div>
             </DetailsWrapper>
           )}
-
         </Wrapper>
       </AuthenticatedLayout>
     </>
